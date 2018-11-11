92df631713ffc3536c263073caab7a9ffa1ebca6
SputnikLQ <SputnikLQ@users.noreply.github.com>
2018-11-11 21:21:35 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+package root;
+
+import java.io.IOException;
+import gui.MainFrame;
+
+public class Main
+{
+	public static void main(String[] args) throws IOException
+	{
+		MainFrame mf = new MainFrame();
+	}
+}
